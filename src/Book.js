79794a6f904274ce5fb5d85e344fe0646438bfa5
Dinesh <dineshreddyy@gmail.com>
2021-04-26 19:37:36 -0700

    added cms functionality

--- src/Book.js
+++ src/Book.js
@@ -7,16 +7,23 @@ import {
   useParams,
 } from "react-router-dom";
 import { Nav } from 'react-bootstrap';
+import { EditorState, ContentState, convertFromHTML } from 'draft-js'
+import { Editor } from 'react-draft-wysiwyg';
+import { convertToRaw } from 'draft-js';
+import draftToHtml from 'draftjs-to-html';
+import '../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 
 const Book = () => {
   const params = useParams();
-  console.log(params);
   const location = useLocation()
   const book = location.state?.book;
   const chapter = location.state?.chapter;
   const [currentChapter,setCurrentChapter] = useState([]);
   let match = useRouteMatch();
    const [mybook,setMybook] = useState([]);
+   const [edit,setEdit] = useState(false);
+   const [ content,setContent]= useState(EditorState.createEmpty());
+   const [git,setGit]=useState('');
  useEffect(() => {
   if(book){
      fetch(book.location)
@@ -24,11 +31,13 @@ const Book = () => {
        .then(
          (result) => {
            setMybook(result);
+               setGit(result[0].location);
                fetch(result[0].location)
                             .then(res => res.json())
                             .then(
                               (result) => {
                                 setCurrentChapter(result);
+                                 updateContent(result.content);
                               },
                               (error) => {
 
@@ -42,11 +51,14 @@ const Book = () => {
        }
    },[]);
     useEffect(() => {
-             if(chapter){  fetch(chapter.location)
+             if(chapter){
+             setGit(chapter.location);
+             fetch(chapter.location)
                  .then(res => res.json())
                  .then(
                    (result) => {
                      setCurrentChapter(result);
+                      updateContent(result.content);
                    },
                    (error) => {
 
@@ -60,6 +72,40 @@ const Book = () => {
                                                                          {chapter.name}
                                                                          </Link>
 </Nav>);
-  return  (<div className="row"><div className="col-xl-2">{mybook.length > 0 && bookTOC}</div><div className="col-xl-8">{ currentChapter && <div><span> Author: {currentChapter.authors}</span><p><div dangerouslySetInnerHTML={{__html: currentChapter.content}} /></p></div> } </div></div>);
+const toggleEdit = () => {
+setEdit(!edit);
+}
+const onEditorStateChange = (content) => {
+   setContent(content);
+}
+const updateContent = (c) => {
+setContent(EditorState.createWithContent(
+                                             ContentState.createFromBlockArray(
+                                                     convertFromHTML(c)
+                                                   )
+                                            ));
+}
+const save = () => {
+const rawContentState = convertToRaw(content.getCurrentContent());
+const markup = draftToHtml(
+  rawContentState
+);
+ let newObject = { "authors": currentChapter.authors,
+                   "content": markup };
+textToClipboard(JSON.stringify(newObject, null, " "));
+window.location.href = git.replace('raw','edit');
+}
+const textToClipboard = (text) => {
+    var dummy = document.createElement("textarea");
+    document.body.appendChild(dummy);
+    dummy.value = text;
+    dummy.select();
+    document.execCommand("copy");
+    document.body.removeChild(dummy);
+}
+   return  (<div className="row"><div className="col-xl-2">{mybook.length > 0 && bookTOC}</div><div className="col-xl-8"><div><button onClick={toggleEdit}>{ edit ? 'Never Mind' : 'Improve this Doc' }</button></div>
+  { currentChapter && !edit && (<div><span> Author: {currentChapter.authors}</span><p><div dangerouslySetInnerHTML={{__html: currentChapter.content}} /></p></div>) }
+   { currentChapter && edit && (<div><Editor editorState={content} wrapperClassName="demo-wrapper" editorClassName="demo-editor" onEditorStateChange={onEditorStateChange} /><button onClick={save}>Save</button></div>) }
+  </div></div>);
 }
 export default Book;
